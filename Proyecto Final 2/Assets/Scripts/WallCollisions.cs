acd1d6d42a4a49541580bdd68809f9b7f7130b2d
JOAQUIN DE FERRARI GONZALEZ <joaquin.deferrari@sansano.usm.cl>
2023-06-03 21:23:21 -0400

    Cambios en anchors de murallas

--- Proyecto Final 2/Assets/Scripts/WallCollisions.cs
+++ Proyecto Final 2/Assets/Scripts/WallCollisions.cs
@@ -15,7 +15,7 @@ public class WallCollisions : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "FireProjectile" || collision.gameObject.tag == "WaterProjectile")
+        if (collision.gameObject.tag == "FireProjectile" || collision.gameObject.tag == "WaterProjectile" || collision.gameObject.tag == "InstaKill")
         {
             Destroy(collision.gameObject);
         }
