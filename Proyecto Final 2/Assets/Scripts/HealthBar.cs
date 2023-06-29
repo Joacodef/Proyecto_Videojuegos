f3a6b76e5415b8e4948ac8aab1852bddbee1308c
Joacodef <joacodef1@gmail.com>
2023-06-28 20:05:09 -0400

    Barra vida flashea en blanco con el daño

--- Proyecto Final 2/Assets/Scripts/HealthBar.cs
+++ Proyecto Final 2/Assets/Scripts/HealthBar.cs
@@ -6,6 +6,25 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public bool flashRed;
+    public float flashRedTime;
+
+    private void Start(){
+        flashRed = false;
+        flashRedTime = 0;
+    }
+
+    private void Update(){
+        if (flashRed){
+            flashRedTime += 1 * Time.deltaTime;
+            slider.fillRect.GetComponent<Image>().color = Color.white;
+            if (flashRedTime >= 0.1){
+                flashRed = false;
+                flashRedTime = 0;
+                slider.fillRect.GetComponent<Image>().color = Color.red;
+            }
+        }
+    }
 
     public void setMaxHealth(float health)
     {
--- Proyecto Final 2/Assets/Scripts/Player.cs
+++ Proyecto Final 2/Assets/Scripts/Player.cs
@@ -65,7 +65,6 @@ public class Player : MonoBehaviour
         {
             currentHealth -= damage;
             healthBar.setHealth(currentHealth);
-            //Debug.Log("Damage taken");
         }
     }
 
